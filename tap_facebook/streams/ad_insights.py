48ab437ada736dbef571fd5ff6b8a2a32644c437
Eliza Nordén <eliza.norden@smartr.se>
2024-10-03 17:27:10 +0200

    Fix

--- tap_facebook/streams/ad_insights.py
+++ tap_facebook/streams/ad_insights.py
@@ -73,12 +73,9 @@ class AdsInsightStream(Stream):
     def schema(self) -> dict:
         properties: th.List[th.Property] = []
 
-        adset_id = self._report_definition["adset_id"]
         date_start = self._report_definition["date_start"]
         date_end = self._report_definition["date_end"]
-        included_fields = [adset_id, date_start, date_end] + self._report_definition[
-            "fields"
-        ]
+        included_fields = [date_start, date_end] + self._report_definition["fields"]
 
         columns = list(AdsInsights.Field.__dict__)[1:]
         for field in columns:
